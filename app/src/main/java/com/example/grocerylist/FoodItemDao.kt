8bc453f6af874a35266c11f17d2dfe72d659d1b2
Courtney <dcourt9@vt.edu>
2019-03-19 21:41:15 -0400

    adddeddd some database stufffffff

---
+++ app/src/main/java/com/example/grocerylist/FoodItemDao.kt
@@ -0,0 +1,30 @@
+package com.example.grocerylist
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface MovieItemDao{
+
+    /** bruh idek
+    @Query("SELECT * FROM movie_table order BY release_date DESC")
+    fun getAllMovies(): LiveData<List<MovieItem>>
+
+
+    @Query ("DELETE FROM movie_table")
+    fun DeleteAll()
+
+    @Insert (onConflict = OnConflictStrategy.REPLACE)
+    fun insertMovie(movie: MovieItem)
+
+
+    @Query("SELECT * FROM movie_table order BY release_date ASC")
+    fun getAllMoviesAsc(): LiveData<List<MovieItem>>
+    **/
+
+
+
+}
